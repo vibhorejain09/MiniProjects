27d23dc83424fecc75ee171f02b3e21b8602e908
vibhorejain09 <vibhorejain0911@gmail.com>
2021-05-12 17:21:52 +0530

    ColorFlipper Done

---
+++ ColorFlipper/hex.js
@@ -0,0 +1,18 @@
+const hex =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function(){
+    //const randomNumber = getRandomNumber(); //get random number in the array colors
+    //console.log(randomNumber);
+    let hexColors = "#cle";
+    for (let i=0; i<6; i++) {
+        hexColors += hex[getRandomNumber()]
+    }
+    document.body.style.backgroundColor = hexColors;
+    color.textContent = hexColors;
+})
+
+function getRandomNumber () {
+    return Math.floor(Math.random() * hex.length);
+}
